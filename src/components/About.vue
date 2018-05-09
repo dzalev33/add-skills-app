83bce83d70af84670e9f4d235d9a7309f851cc44
dzalev33 <dzalev33@gmail.com>
2018-05-09 15:04:54 +0200

    first commit

---
+++ src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="about">
+    <h1>Hello {{their_name}},This is an About Page</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur cupiditate delectus esse eveniet harum, illum impedit perspiciatis quis voluptates! Accusamus blanditiis delectus distinctio id labore odio porro tempore temporibus voluptatibus!</p>
+</div>
+</template>
+
+<script>
+    export default {
+        name: 'Skills',
+        data () {
+            return {
+                their_name: this.$route.params.name
+            }
+        },
+    }
+</script>
+
